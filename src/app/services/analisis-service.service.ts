aec3a7ebe782c01869550b071590d3132cd2a06f
Freddy <ferreirafreddy39@gmail.com>
2023-03-16 23:23:38 -0300

    prod

--- src/app/services/analisis-service.service.ts
+++ src/app/services/analisis-service.service.ts
@@ -13,7 +13,7 @@ export class AnalisisService {
   private cargando =  true;
   public analisis: IAnalisis[];
   public analisisSeleccionados: IAnalisis[] = [];
-  public urlEndPoint:string = 'http://localhost:8080/api/analisis';
+  public urlEndPoint:string = 'https://labweb-production.up.railway.app/api/analisis';
 
   constructor(private http: HttpClient) { 
     this.cargarAnalisis();
